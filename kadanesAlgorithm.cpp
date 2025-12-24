fbfb7176197aaa862d366bb85232491e83ddb76e
Prabhat Bidalia <prabhatbidalia26@gmail.com>
2025-12-24 13:07:07 +0530

    Refactor to use maxSubarraySum function

--- kadanesAlgorithm.cpp
+++ kadanesAlgorithm.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++) cin>>arr[i];
-    int currSum = 0;
-    int maxSum = 0;
-    for(int i = 0; i < n; i++){
-        currSum += arr[i];
-        maxSum = max(currSum, maxSum);
-        if(currSum < 0){
-            currSum = 0;
+int maxSubarraySum(const vector<int>& arr) {
+    int currentSum = 0;
+    int maxSum = INT_MIN;
+    for (int x : arr) {
+        currentSum += x;
+        maxSum = max(maxSum, currentSum);
+        if (currentSum < 0) {
+            currentSum = 0;
         }
     }
-    cout<<maxSum<<endl;
+    return maxSum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << maxSubarraySum(arr) << endl;
+    return 0;
 }
